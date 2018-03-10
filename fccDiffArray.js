b417a1be2037e32623e9198c30c9bf6bb3de9be9
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-03-09 23:21:02 -0500

    Attempt 1

---
+++ fccDiffArray.js
@@ -0,0 +1,19 @@
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  // return element that doesn't exist in both arrays
+
+  for(var i = 0; i < arr1.length; i++) {
+    //scan through
+    var el1 = arr1[i];
+
+    for(var j = 0; j < arr2.length; j++) {
+      
+    }
+  }
+
+  return newArr;
+}
+
+var result = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+console.log(result);
