77fbbf8abe057be37aa1d6f7037bfab0c16168d4
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-04-13 21:14:19 -0400

    finish lhlabs day 14
    Make isDangerous() that finds currents / rocks in the top, bottom, left, right of the given grid letter

--- lhlabsGridSize.js
+++ lhlabsGridSize.js
@@ -1,6 +1,8 @@
 /*
-Challenge 13
-That was outstanding! Now do the same thing with firstCurrent(). Don't reinvent the wheel here. Re-use existing code from Challenge #11. This is one of the most important skills you can build as a coder...er, uh, I mean lighthouse operator.
+Challenge 14
+Ship captains are really starting to rely on you to plot their routes through your area. Your name is being sung in pubs and taverns up and down the coast. And this time, it's for the right reasons! The ship captains are starting to be curious about some of the cells in your grid, and what they want is to know if a cell is dangerous. They know if ones are safe, but sometimes, they are willing to accept a bit of danger so that they can get a faster delivery time.
+
+A cell is considered dangerous if there is a rock or a strong current in it, OR in the cells immediately above, below, left, or right of it. Write a function called isDangerous() that will take a cell in the format 'G7' and return a true or a false value. (Example: isDangerous('E4') would return true, because there is a rock there. Similarly, isDangerous('B9') would return true, because there are rocks and currents AROUND the cell. However, isDangerous('I6') would return false because it is open water.)
 */
 
 const GRID = [
@@ -39,7 +41,7 @@ function lightCell(cell) {
   var cellValid = true;
 
   //count cell length - 1 (index based), flag as false if cell invalid
-  if (y > GRID.length - 1 || x > GRID[y].length - 1) {
+  if (y < 0 || y > GRID.length - 1 || x < 0 || x > GRID[y].length - 1) {
     cellValid = false;
   }
 
@@ -50,12 +52,14 @@ function lightCell(cell) {
 
 
 function isRock(cell) {
-  return lightCell(cell) === '^';
+  var cellContent = lightCell(cell);
+  return cellContent ? cellContent === '^' : false;
 }
 
 
 function isCurrent(cell) {
-  return lightCell(cell) === '~';
+  var cellContent = lightCell(cell);
+  return cellContent ? cellContent === '~' : false;
 }
 
 
@@ -156,4 +160,19 @@ function firstCurrent() {
   return allCurrents()[0];
 }
 
-console.log(firstCurrent());
+
+function isDangerous(cell) {
+  // check if grid exist
+  var cellX = cell[0];
+  var cellY = Number(cell.substr(1));
+
+  // get letters
+  var top = ! isSafe(`${cellX}${cellY - 1}`);
+  var bottom = ! isSafe(`${cellX}${cellY + 1}`);
+  var left = ! isSafe(`${String.fromCharCode(cellX.charCodeAt(0) - 1)}${cellY + 1}`);
+  var right = ! isSafe(`${String.fromCharCode(cellX.charCodeAt(0) + 1)}${cellY + 1}`);
+
+  return top || bottom || left || right;
+}
+
+console.log(isDangerous('A1'));
