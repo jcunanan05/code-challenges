dfc17b0b0dff95b3260345b2d11bae5c283bd9e6
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-05-14 10:25:59 -0400

    Finish diagonal difference challenge
    compute the diagonal differences between the matrix

--- hackerrank.js
+++ hackerrank.js
@@ -1,16 +1,23 @@
-//compare alice vs bob's numbers and score them
-function solve(a0, a1, a2, b0, b1, b2) {
-  var alice = Array.from(arguments).splice(0,3);
-  var bob = Array.from(arguments).splice(3,5);
-  var aliceScore = 0;
-  var bobScore = 0;
+//print absolute value difference of diagonals in the matrix
 
-  alice.forEach((a, i) => {
-    if(a > bob[i]) aliceScore += 1;
-    else if(a < bob[i]) bobScore += 1;
-  });
+function diagonalDifference(a) {
+  let firstDiagonal = a.reduce((prev, current, i) => {
+    return prev + current[i];
+  }, 0);
 
-  return [aliceScore, bobScore];
+  let secondDiagonal = a.reduce((prev, current, i) => {
+    return prev + current.reverse()[i];
+  }, 0);
+
+  return Math.abs(firstDiagonal - secondDiagonal);
 }
 
-console.log(solve(5, 6, 7, 3, 6, 10));
+//must be equal to 0
+
+let a = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+console.log(diagonalDifference(a));
