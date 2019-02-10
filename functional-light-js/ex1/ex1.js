185745321ed1b9d5165151d41b8355df896f1005
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-10 00:55:34 -0500

    Complete exercise 1

--- functional-light-js/ex1/ex1.js
+++ functional-light-js/ex1/ex1.js
@@ -1,12 +1,40 @@
 function foo(x) {
-	y++;
-	z = x * y;
+  y++;
+  z = x * y;
 }
 
-var y = 5, z;
+var y = 5,
+  z;
 
 foo(20);
-z;		// 120
+z;
+// 120
 
 foo(25);
-z;		// 175
+z;
+// 175
+
+// pure wrapper
+function pureBar(x, y) {
+  var z;
+  foo(x);
+  return [y, z];
+  function foo(x) {
+    y++;
+    z = x * y;
+  }
+}
+
+console.log('pure function: ', pureBar(20, 5));
+
+// pure side-effect
+function pureSideEffectBar(curX, curY) {
+  var [origY, origZ] = [y, z];
+  y = curY;
+  foo(curX);
+  var [newY, newZ] = [y, z];
+  [y, z] = [origY, origZ];
+  return [newY, newZ];
+}
+
+console.log('pure side-effect: ', pureSideEffectBar(20, 5));
