ec61a95a4b437e4928a0258f67b3501ec3975ea2
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-05-24 17:00:43 -0400

    Add another problem

--- any/staircase.js
+++ any/staircase.js
@@ -1,12 +1,28 @@
 function staircase(stairSteps) {
+  function fibonnaci(n) {
+    if(n <= 1) return 1;
+    else {
+      return fibonnaci(n - 1) + fibonnaci(n - 2);
+    }
+  }
   return fibonnaci(stairSteps);
 }
 
-function fibonnaci(n) {
-  if(n <= 1) return 1;
-  else {
-    return fibonnaci(n - 1) + fibonnaci(n - 2);
+/**
+ * 
+ * @param {Number} stairSteps 
+ * @param {Number[]} stepList 
+ */
+function variableStepsStaircase(stairSteps, stepList) {
+  function countSteps(step) {
+    if (step > stairSteps) return 0;
+    if (step === stairSteps) return 1;
+    if (step <= 1) return 1;
+    return countSteps(stairSteps - step)
   }
+
+  return stepList.reduce((accumulator, currentValue) => accumulator + countSteps(currentValue))
 }
 
+exports.variableStepsStaircase = variableStepsStaircase;
 module.exports = staircase
