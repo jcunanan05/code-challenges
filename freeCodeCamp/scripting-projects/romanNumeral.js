1e6f26f5ab5c7d2a8f7d61236a4aa62808a05470
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-08 22:54:08 -0400

    Add roman numeral exercise

--- freeCodeCamp/scripting-projects/romanNumeral.js
+++ freeCodeCamp/scripting-projects/romanNumeral.js
@@ -1,5 +1,37 @@
-function romanNumeral() {
-  return "I";
+const ROMAN_VALUES = {
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000
+};
+
+const ROMAN_NUMERALS = Object.keys(ROMAN_VALUES).reverse();
+
+function romanNumeral(num) {
+  let romanResult = "";
+  let numCopy = (() => num)();
+  // loop over roman numerals
+  ROMAN_NUMERALS.forEach(romanNumeral => {
+    // scan if number is greater than roman numeral
+    while (numCopy >= getDecimal(romanNumeral)) {
+      romanResult += romanNumeral;
+      numCopy -= getDecimal(romanNumeral);
+    }
+  });
+  return romanResult;
+}
+
+function getDecimal(romanNumeral) {
+  return ROMAN_VALUES(romanNumeral);
 }
 
 module.exports = romanNumeral;
