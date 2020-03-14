afe66d54b094ab61ffce6fc0f5b6276844d92cf2
jcunanan05 <cunanan.jonathan1995@gmail.com>
2020-03-13 22:28:30 -0400

    add fcc

---
+++ freeCodeCamp/intermediate-scripting/diffTwoArrays.js
@@ -0,0 +1,23 @@
+/**
+ * 
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+Note
+You can return the array with its elements in any order. 
+
+ */
+
+function diffArray(arr1, arr2) {
+  const diff1 = arr1.filter(el => {
+    return !arr2.includes(el);
+  });
+
+  const diff2 = arr2.filter(el => {
+    return !arr1.includes(el);
+  });
+
+  return [...diff1, ...diff2];
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
